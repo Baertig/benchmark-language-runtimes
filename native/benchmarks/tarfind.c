df0a9f43b4bc0756706a8ed38fc335da42dcb398
Georg Bär-Dumont <georg.baerdumont@gmail.com>
2025-10-16 11:06:17 +0200

    feat(native): configure ITERATIONS and SCALE_FACTOR

--- native/benchmarks/tarfind.c
+++ native/benchmarks/tarfind.c
@@ -12,7 +12,9 @@
 #include <stdint.h>
 #include "support.h"
 
-#define LOCAL_SCALE_FACTOR 46
+#ifndef SCALE_FACTOR
+#define SCALE_FACTOR 1
+#endif
 
 // number of files in the archive
 #define ARCHIVE_FILES 35
@@ -25,77 +27,64 @@
 
 // this is the basic TAR header format which is in ASCII
 typedef struct {
-  char filename[100];
-  char mode[8];  // file mode
-  char uID[8];   // user id
-  char gID[8];   // group id
-  char size[12]; // in bytes octal base
-  char mtime[12]; // numeric Unix time format (octal)
-  char checksum[8]; // for the header, ignored herew2
-  char isLink;
-  char linkedFile[100];
+	char filename[100];
+	char mode[8];  // file mode
+	char uID[8];   // user id
+	char gID[8];   // group id
+	char size[12]; // in bytes octal base
+	char mtime[12]; // numeric Unix time format (octal)
+	char checksum[8]; // for the header, ignored herew2
+	char isLink;
+	char linkedFile[100];
 } tar_header_t;
 
-static int benchmark_body(unsigned int lsf, unsigned int gsf);
+int __attribute__ ((noinline)) benchmark() {
+	int i, p;
+	tar_header_t * hdr;
+	int found = 0;
+	unsigned sf = SCALE_FACTOR;
 
+	for (unsigned int sf_cnt = 0; sf_cnt < sf; sf_cnt++) {
 
-int
-benchmark (void)
-{
-  return benchmark_body (LOCAL_SCALE_FACTOR, GLOBAL_SCALE_FACTOR);
-}
+		// always create ARCHIVE_FILES files in the archive
+		int files = ARCHIVE_FILES;
+		hdr = malloc(sizeof(tar_header_t) * files);
+		for (i = 0; i < files; i++){
+			// create record
+			tar_header_t * c = & hdr[i];
+			// initialize here for cache efficiency reasons
+			memset(c, 0, sizeof(tar_header_t));
+			int flen = 5 + i % 94; // vary file lengths
+			c->isLink = '0';
+			for(p = 0; p < flen; p++){
+				c->filename[p] = rand() % 26 + 65;
+			}
+			c->size[0] = '0';
+		}
 
+		found = 0; // number of times a file was found
+		// actual benchmark, strcmp with a set of N_SEARCHES files
+		// the memory access here is chosen inefficiently on purpose
+		for (p = 0; p < N_SEARCHES; p++){
+			// chose the position of the file to search for from the mid of the list
+			char * search = hdr[(p + ARCHIVE_FILES/2) % ARCHIVE_FILES].filename;
 
-static int __attribute__ ((noinline))
-benchmark_body(unsigned int lsf, unsigned int gsf)
-{
-  int i, p;
-  tar_header_t * hdr;
-  int found = 0;
-
-  for (unsigned int lsf_cnt = 0; lsf_cnt < lsf; lsf_cnt++)
-    for (unsigned int gsf_cnt = 0; gsf_cnt < gsf; gsf_cnt++)
-      {
-
-	// always create ARCHIVE_FILES files in the archive
-	int files = ARCHIVE_FILES;
-	hdr = malloc(sizeof(tar_header_t) * files);
-	for (i = 0; i < files; i++){
-	  // create record
-	  tar_header_t * c = & hdr[i];
-	  // initialize here for cache efficiency reasons
-	  memset(c, 0, sizeof(tar_header_t));
-	  int flen = 5 + i % 94; // vary file lengths
-	  c->isLink = '0';
-	  for(p = 0; p < flen; p++){
-	    c->filename[p] = rand() % 26 + 65;
-	  }
-	  c->size[0] = '0';
+			// for each filename iterate through all files until found
+			for (i = 0; i < files; i++){
+				tar_header_t * cur = & hdr[i];
+				// implementation of strcmp
+				char *c1;
+				char *c2;
+				for (c1 = hdr[i].filename, c2 = search; (*c1 != '\0' && *c2 != '\0' && *c1 == *c2) ; c1++, c2++);
+				// complete match?
+				if(*c1 == '\0' && *c2 == '\0'){
+					found++;
+					break;
+				}
+			}
+		}
+		free(hdr);
 	}
 
-	found = 0; // number of times a file was found
-	// actual benchmark, strcmp with a set of N_SEARCHES files
-	// the memory access here is chosen inefficiently on purpose
-	for (p = 0; p < N_SEARCHES; p++){
-	  // chose the position of the file to search for from the mid of the list
-	  char * search = hdr[(p + ARCHIVE_FILES/2) % ARCHIVE_FILES].filename;
-
-	  // for each filename iterate through all files until found
-	  for (i = 0; i < files; i++){
-	    tar_header_t * cur = & hdr[i];
-	    // implementation of strcmp
-	    char *c1;
-	    char *c2;
-	    for (c1 = hdr[i].filename, c2 = search; (*c1 != '\0' && *c2 != '\0' && *c1 == *c2) ; c1++, c2++);
-	    // complete match?
-	    if(*c1 == '\0' && *c2 == '\0'){
-	      found++;
-	      break;
-	    }
-	  }
-	}
-	free(hdr);
-      }
-
-  return found == N_SEARCHES;
+	return found == N_SEARCHES;
 }
